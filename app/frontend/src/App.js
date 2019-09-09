5cc0ec8a0d2a0280fbc670d990bb14b8a5e53d41
Joseph Myalla <chanangae@gmail.com>
2019-09-09 14:35:41 +0300

    tailwindcss configuration

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,24 +1,36 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function App () {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+  <div className="flex items-center flex-shrink-0 text-white mr-6">
+    <span className="font-semibold text-xl tracking-tight">Chingu Chat App</span>
+  </div>
+  <div className="block lg:hidden">
+    <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    </button>
+  </div>
+  <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div className="text-sm lg:flex-grow">
+      <a href="http://chingu.io" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Sign Up
+      </a>
+      <a href="http://chingu.io" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Sing In
+      </a>
+      <a href="http://chingu.io" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+        About Us
+      </a>
+    </div>
+ </div>
+</nav>
       </header>
+      <div className="px-8 py-4">
+        Welcome to Chingu Chat App
+      </div>
     </div>
   )
 }
