60968d641dfaccd1561fdd1072969fdedb2bbc7e
Joseph Myalla <chanangae@gmail.com>
2019-09-10 11:37:19 +0300

    update static layout ui

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -24,39 +24,38 @@ function App () {
   </div>
   
   <div className='flex-1 flex flex-col min-h-screen h-screen'>
-    <div>
-      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">Chingu|Chat App</span>
+    <div className="flex">
+      <div className="w-56 bg-gray-800 border-b border-teal-500 flex items-center flex-shrink-0 text-white px-4"><span className="font-semibold text-xl tracking-tight">Chingu|Chat App</span></div>
+      <div className="flex-1">
+      <div className="flex items-center bg-gray-100 justify-between border-b border-teal-500">
+      <div className="flex items-center flex-shrink-0 text-blue-800 mr-6 px-4">
+        <span className="font-semibold text-xl tracking-tight"># General Chat</span>
       </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+    <nav className="flex items-center justify-between flex-wrap p-6">
+    <div className="block lg:hidden">
+        <button className="flex items-center px-3 py-2 border rounded text-blue-800 hover:text-yellow-400 hover:border-white">
           <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
         </button>
       </div>
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
-          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-            About Chingu
-          </a>
-        </div>
-        <div>
-          <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Sign In</a>
-          <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Sign Up</a>
-          <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">Logout</a>
+      <div>
+          <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-blue-800 hover:text-yellow-400 mr-4">Sign In</a>
+          <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-blue-800 hover:text-yellow-400 mr-4">Sign Up</a>
+          <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-blue-800 hover:text-yellow-400 mr-4">Logout</a>
         </div>
       </div>
     </nav>
+      </div>
+      </div>
     </div>
          
     <div className="flex-1 bg-gray-900 flex overflow-y-hidden">
     
           <div className="bg-gray-800 text-white px-2 py-2 w-56 flex-none flex flex-col justify-between">
             <div className="overflow-y-auto">
-              Hashtag SideBar, 
-              
+             # Bears 11
             </div>
-            <div className="">
+            <div className="flex">
               Bottom Sidebar
             </div>
           </div>
@@ -64,19 +63,19 @@ function App () {
             <div className="bg-gray-100 px-4 py-2 flex-1 flex flex-col justify-between">
               <div className="overflow-y-auto">
               
-              <div className="flex mt-4">
+              <div className="flex mt-4 border-b py-4 border-green-300">
      
               <div>
                   <img src="../imgs/img1.jpg" className="h-12 w-12 rounded-full"/>
                 </div>
                  <div className="flex flex-col ml-4">
                    <p className="font-bold">Goldino<span className="ml-4 font-normal text-gray-500 xsm">Today 08:00 AM</span></p>
-                   <p className="text-gray-600">when did you joing chingu @jmmedlock</p>
+                   <p className="text-gray-600">when did you join chingu @jmmedlock</p>
                  </div>
 
               </div>
 
-              <div className="flex mt-4">
+              <div className="flex mt-4 border-b py-4 border-green-300">
      
                 <div>
                   <img src="../imgs/img0.jpg" className="h-12 w-12 rounded-full" />
@@ -88,7 +87,7 @@ function App () {
 
               </div>
 
-              <div className="flex mt-4">
+              <div className="flex mt-4 border-b py-4 border-green-300">
               <div>
                   <img src="../imgs/img1.jpg" className="h-12 w-12 rounded-full" />
                 </div>
@@ -97,18 +96,42 @@ function App () {
                    <p className="font-bold">sojelee<span className="ml-4 font-normal text-gray-500 xsm">Today 45:00 PM</span></p>
                    <p className="text-gray-600">Hollow team</p>
                  </div>
-
               </div>
-
-              
-              
             </div>
-            <div>
-              Chat Input Always Bottom
+            <div className="bg-gray-300 h-12 flex items-center">
+               <div className="w-12 px-2 py-2 border-r border-gray-100">Left</div>
+               <div className="flex-1 px-4 py-2">
+                 
+               <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-1 px-2 block w-full appearance-none leading-normal" type="text" placeholder="Chat # Bears 11" />
+              </div>
+               <div className="px-2 py-2">Emojis</div>
             </div>
             </div>
             <div className="w-56 bg-gray-300 px-2 py-2 flex-none overflow-y-auto">
-              User SideBar,
+            <div className="px-2 py-2">Online-2</div>
+            <div className="flex mt-2">
+            <div className="relative">
+                  <img src="../imgs/img1.jpg" className="h-10 w-10 rounded-full" />
+                  <div className="absolute h-4 w-4 bg-green-500 rounded-full m-6 top-0"></div>
+                </div>
+              <div className="flex flex-col ml-2">
+                 <p className="font-bold mt-1">sojelee</p>
+              </div>
+
+              </div>
+
+              <div className="flex mt-2">
+              <div className="relative">
+                  <img src="../imgs/img0.jpg" className="h-10 w-10 rounded-full"/>
+                  <div className="absolute h-4 w-4 bg-green-500 rounded-full m-6 top-0"></div>
+                </div>
+              <div className="flex flex-col ml-2">
+                 <p className="font-bold mt-1">chance</p>
+              </div>
+
+              </div>
+
+
             </div>
           </div>
     
