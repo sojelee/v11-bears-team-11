e3980bb05b6ae9a0b768d3b203b06850f9ad03ec
Joseph Myalla <chanangae@gmail.com>
2019-09-16 15:39:35 +0300

    ui update

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Layout from './components/layout/Layout';
-import Channel from './components/layout/Channel';
 
-function App () {
+export default function App () {
   return (
-  <Layout />
+ <Layout />
 )
 }
 
-export default App
